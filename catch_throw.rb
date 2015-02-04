ac81f08736f978dd13f6603a454ab4f0b8b0cdd7
Maximilianoho <maximho20@gmail.com>
2015-02-04 20:58:43 +0100

    catch and touch sample 2

--- catch_throw.rb
+++ catch_throw.rb
@@ -6,3 +6,15 @@ catch (:finish) do
 
 	puts "Generated 1000 random numbers without generating 123!"
 end
+
+#catch and touch separate sample:
+#
+#def generate_random_number_except_123
+#	x = rand(1000)
+#	throw :finish if x == 123
+#end
+
+#catch(:finish) do
+#	1000.times { generate_random_number_except_123 }
+#	puts "Generated 1000 random numbers without generating 123!"
+#end
